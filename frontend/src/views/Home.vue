8877b3565aa95c0a482fca27f99083e7318b9965
KyungWan <wanni26@naver.com>
2020-11-09 10:56:22 +0900

    views_update

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,62 +1,106 @@
-<template>
-  <Layout>
-  </Layout>
+<template v-if="isLogedIn">
+<v-responsvie style="aspect-ratio: 16:9">
+  <v-main>
+    <Layout>
+      <template #content>
+        <v-container class="header">
+          <v-parallax src="@/assets/testing.jpg">
+            <p v-scrollanimation>매일 똑같은 노래를 듣는 당신에게</p>
+            <p v-scrollanimation>한번의 클릭만으로 자기 자신을 찾아보세요</p>
+          </v-parallax>
+          <div class="button">
+            <v-btn next to="/upload">시작하기</v-btn>
+          </div>
+        </v-container>
+        <v-parallax src="@/assets/3.jpg" height="750">
+          <v-row align="left" justify="left" text-align="left">
+            <v-col class="text-center" cols="12">
+              <h1>
+                딥러닝 기반으로한 음악 추천 서비스
+              </h1>
+              <h4 class="subheading">
+                5000가지 이상의 데이터로 당신의 취향을 분석해드립니다
+              </h4>
+            </v-col>
+          </v-row>
+        </v-parallax>
+        <div id="info">
+        </div>
+        <v-parallax src="@/assets/4.jpg" max-height="500px"></v-parallax>
+      </template>
+    </Layout>
+  </v-main>
+</v-responsvie>
 </template>
 
 <script>
 import Layout from '../components/Layout'
-
-// @ is an alias to /src
-/* eslint-disable no-unused-vars */
-import store from '../store'
-import Vue from 'vue'
-import router from '../router'
-
+import {
+  mapState,
+  mapGetters
+} from 'vuex'
 export default {
-  components: { Layout },
-  methods: {
-    Success () {
-      (window.location.pathname !== '/Music') ? router.push('/Music') : router.go(0)
-    }
+  components: {
+    Layout
+  },
+  computed: {
+    ...mapState({
+      lists: state => state.lists
+    }),
+    ...mapGetters([
+      'isLogedIn'
+    ])
   }
 }
 </script>
 
 <style>
-  .border-styles > p{
-    margin: 2px 0;
-    padding: 1px 3px;
-    border-width: 2px;
-    border-color: #aaa;
-  }
-  body {
-    font-family: Helvetica Neue, Arial, sans-serif;
-    font-size: 14px;
-    color: #444;
-  }
+div {
+  /* border: 2px solid black; */
+}
 
-  table {
-    border: 2px solid #42b983;
-    border-radius: 3px;
-    background-color: #fff;
-  }
+#text {
+  height: 60px;
+  padding: 20px;
+  color: black;
+}
 
-  th {
-    background-color: #42b983;
-    color: rgba(255, 255, 255, 0.66);
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -user-select: none;
-  }
+#info {
+  height: 150px;
+  padding: 20px;
+  color: #90A4AE;
+}
 
-  td {
-    background-color: #f9f9f9;
-  }
+#content {
+  height: auto;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
 
-  th,
-  td {
-    min-width: 120px;
-    padding: 10px 20px;
-  }
+.header {
+  font-size: 75px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-weight: 900;
+  max-width: 100%;
+  height: 37%;
+  text-align: left;
+  padding: 0px;
+  margin-top: 10%;
+}
+
+.button {
+  height: 60px;
+  text-align: center;
+  margin-top: 9%;
+}
+
+.before-enter {
+  opacity: 0;
+  transform: translateX(500px);
+  transition: all 2.5s ease-out;
+}
+
+.enter {
+  opacity: 1;
+  transform: translateX(0px);
+}
 </style>
--- frontend/src/views/analysis.vue
+++ frontend/src/views/analysis.vue
@@ -1,71 +1,46 @@
 <template>
   <Layout>
-    <template>
-        <v-app-bar
-          dark
-        >
-        <!-- 타이틀 -->
-          <v-toolbar-title><div @click="home">음악 분석</div></v-toolbar-title>
-          <v-spacer></v-spacer>
-        </v-app-bar>
-    </template>
-    <template>
-      <v-responsive style="aspect-ratio: 16:9">
-        <v-main id="inspire">
-          <v-container fluid style="padding: 5px">
-          <!-- Python 연결해서 스펙트럼 들어갈 자리 -->
-            <v-img src="@/assets/music.jpeg" max-height="270px"> </v-img>
-            <div align="center">
-              <v-data-table style="width: auto"
-                            :headers="headers"
-                            :items="lists"
-                            >
-               <template #item.title="{ item }">
-                  <a :href="item.address" target=_blank>{{ item.title }}</a>
-               </template>
-              </v-data-table>
-
-              <v-btn outlined next to="/recommendation" color="gray"
-                           style="padding: auto width: auto">
-                비슷한 음악 찾기
-              </v-btn>
-            </div>
-          </v-container>
-        </v-main>
-      </v-responsive>
+    <template #content>
+        <template>
+          <v-responsive style="aspect-ratio: 16:9">
+            <v-main id="inspire">
+              <v-container fluid style="padding: 5px">
+                <button v-on:click="getData">이미지 가져오기</button>
+              </v-container>
+            </v-main>
+          </v-responsive>
+        </template>
     </template>
   </Layout>
 </template>
 
 <script>
+import Layout from '../components/Layout'
+// @ is an alias to /src
+/* eslint-disable no-unused-vars */
+import store from '../store'
+import Vue from 'vue'
 import router from '../router'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  data: () => ({
-    headers: [
-      { text: '제목', value: 'title' },
-      { text: '장르', value: 'genre' },
-      { text: '키워드', value: 'keyword' },
-      { text: '사용된 악기', value: 'instrument' }
-    ]
-  }),
   methods: {
-    // (리스트) 홈으로 이동 등등...
-    home () {
-      (window.location.pathname !== '/') ? router.push('/') : router.go(0)
+    getData: function () {
+      axios.get('http://127.0.0.1:5000/upload')
+        .then(function (res) {
+          console.log(res)
+        }).catch(function (e) {
+          console.error(e)
+        })
     }
+  },
+  components: {
+    Layout
   }
+
 }
 </script>
 
-<style scoped>
-  div {
-    //border: 2px solid black;
-    font-family: Ubuntu;
-    text-align: center;
-    //text-decoration: underline;
-    margin: 10px;
-    padding: 20px;
-  }
+<style>
+
 </style>
